1d43231b020b39440eeabee3ea73cb45acea3c07
Melania Morales <melaniacrc@gmail.com>
2021-08-09 21:53:41 -0600

    C: Path input from user

--- procesador.py
+++ procesador.py
@@ -4,6 +4,11 @@ def readFile(path):
     file1 = open(path,"r")
     return file1.read()
 
+def takePath():
+    path = input("Indique el path donde se encuentra su archivo: ")
+    return path
 
-text = readFile("C:/University/Recuperacion de informacion textual/Tarea 1/University-SemesterVI-WordProcessor/pruebas/test.txt")
+path = takePath()
+text = readFile(path)
+
 print(text)
